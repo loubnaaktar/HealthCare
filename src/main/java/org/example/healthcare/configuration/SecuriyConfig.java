0b2fbe3ebb993162ef885780065205edc5d5d052
loubnaaktar <loubnaakt13@gmail.com>
2026-05-19 17:51:19 +0100

    autorisation admin

--- src/main/java/org/example/healthcare/configuration/SecuriyConfig.java
+++ src/main/java/org/example/healthcare/configuration/SecuriyConfig.java
@@ -1,12 +1,15 @@
 package org.example.healthcare.configuration;
 
+import io.swagger.v3.oas.models.PathItem;
 import lombok.RequiredArgsConstructor;
 import org.example.healthcare.exception.GlobalExceptionHandler;
 import org.example.healthcare.security.CustomUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -15,22 +18,45 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
+@EnableMethodSecurity
+
 public class SecuriyConfig {
+
     private final CustomUserDetailsService userDetailsService;
 
      @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http, GlobalExceptionHandler.JwtFilter jwtFilter) throws Exception {
 
-        http.csrf(AbstractHttpConfigurer::disable)
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(auth -> auth.requestMatchers("/api/auth/**").permitAll()
-                        .anyRequest().authenticated())
-                .addFilterBefore(jwtFilter,UsernamePasswordAuthenticationFilter.class);
-        return http.build();
+         http
+                 .cors(c -> {
+                     CorsConfigurationSource source = corsConfigurationSource();
+                     c.configurationSource(source);
+                 })
+                 .csrf(AbstractHttpConfigurer::disable)
+                 .authorizeHttpRequests(auth -> {
+                     auth.requestMatchers(
+                                     "/api/auth/**"
+                             ).permitAll()
+                             .requestMatchers(HttpMethod.GET, "/api/patient","/api/medecin").hasRole("ADMIN")
+                             .requestMatchers(HttpMethod.POST,"/api/medecin/ajouter","/api/patient/ajouter").hasRole("ADMIN")
+                             .requestMatchers(HttpMethod.PUT,"/api/medecin/modifier/{id}","/api/patient/modifier/{id}").hasRole("ADMIN")
+                             .requestMatchers(HttpMethod.DELETE,"/api/medecin/modifier/{id}","/api/patient/modifier/{id}").hasRole("ADMIN")
+                             .anyRequest()
+                             .authenticated();
+                 })
+                             .addFilterBefore(jwtFilter,UsernamePasswordAuthenticationFilter.class)
+                             .authenticationManager(authenticationManager(http,passwordEncoder()));
+
+         return http.build();
      }
 
      @Bean
@@ -44,4 +70,17 @@ public class SecuriyConfig {
          authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
          return authenticationManagerBuilder.build();
      }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:8080"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 }
